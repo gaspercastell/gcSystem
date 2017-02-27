f74da4488cc212f01a6db54f86f82746a1e47fd5
gaspercastell <retro.rever@gmail.com>
2017-02-26 22:55:04 -0600

    se agrego controller para creación de servicios web de usuarios

--- gcsystemback/config/routes.rb
+++ gcsystemback/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  #resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+
+  namespace :api do
+    resources :users, only: [:index, :create, :destroy, :update, :show]
+  end
+
 end
