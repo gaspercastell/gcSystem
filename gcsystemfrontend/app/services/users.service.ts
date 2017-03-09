24f146aea519f1f77bae44fa76905ca7ec14624f
gaspercastell <retro.rever@gmail.com>
2017-03-09 13:42:56 -0600

    se agrego cors al back para que se pudiera ejecutar el service web en el front

--- gcsystemfrontend/app/services/users.service.ts
+++ gcsystemfrontend/app/services/users.service.ts
@@ -6,9 +6,11 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class UserService{
     constructor(private _http:Http){}
+    
     getCurrentTime(){
         return this._http.get('http://localhost:3000/api/users.json')
         //return this._http.get('http://time.jsontest.com')
+        //return this._http.post('http://localhost:3000/api/users.json/',"")
         .map(res => res.json());
     }
 }
